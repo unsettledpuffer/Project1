91c3f507b288eda631332e52572f3ca97779ac18
sammy <sammylynch488@gmail.com>
2026-03-05 10:53:25 +0000

    -Added drawlogic and errorchecks -Added "clear console" to clear previous output -Refined certain code

--- main.py
+++ main.py
@@ -2,6 +2,17 @@
 
 import numpy as np
 
+#errorchecks
+def typeCheck():
+    while True:
+        try:
+            playerMove = int(input("Which column would you like to drop in?"))
+            return playerMove
+        except ValueError:
+            print("Invalid character entered")
+
+#generation
+
 def createBoard():
     return np.zeros((6,7),dtype=int)
 
@@ -10,14 +21,18 @@ board = createBoard()
 def displayBoard():
     print(board)
 
-
+def clearConsole():
+    for x in range(10):
+        print(" ")
 #player 1 yellow
 #player 2 red
 
 def getPlayerMove():
-    playerMove = int(input("Which column would you like to drop in?"))
+    playerMove = typeCheck()
+    while playerMove > 7 or playerMove < 1:
+        print("That is not a column. Re enter")
+        playerMove = typeCheck()
     return playerMove
-
 
 def getColumnLowestSlot(playerInput):
     for row in range(5,-1,-1):
@@ -27,25 +42,45 @@ def getColumnLowestSlot(playerInput):
             pass
     return False, 0, 0
 
-displayBoard()
+#wincheck
+def winCheck(currentPlayersTurn):
+    win = False
+    #draw
+    if not (board == 0).any() and win == False:
+        return "Draw"
+    #win
 
+    
+
+#main
 gameIsNotWon = True
-currentPlayersTurn = "one"
+currentPlayersTurn = 1
+clearConsole()
+displayBoard()
 while gameIsNotWon:
+    print("It is currently the turn of player",currentPlayersTurn)
 
     ifDecider, row, playerInput = getColumnLowestSlot(getPlayerMove())
 
-    if currentPlayersTurn.lower() == "one":
+    if currentPlayersTurn == 1:
         if ifDecider:
             board[row][playerInput-1] = "1"
-            currentPlayersTurn = "two"
+            currentPlayersTurn = 2
+            clearConsole()
             displayBoard()
+            if winCheck(currentPlayersTurn) == "Draw":
+                print("Game is a draw")
+                break
         else:
             print("You cannot move here, please try again")
-    elif currentPlayersTurn.lower() == "two":
+    elif currentPlayersTurn == 2:
         if ifDecider:
             board[row][playerInput-1] = "2"
-            currentPlayersTurn = "one"
+            currentPlayersTurn = 1
+            clearConsole()
             displayBoard()
+            if winCheck(currentPlayersTurn) == "Draw":
+                print("Game is a draw")
+                break
         else:
             print("You cannot move here, please try again")
